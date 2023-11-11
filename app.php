a65996db3a7a7b78abb63b038692b2c1f56884ca
shahram <ardalan.shahram@gmail.com>
2023-11-11 20:52:02 +0330

    add php and css file to project

---
+++ app.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app.css">
+    <title>My First PHP App</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1><?php echo "How AI-Generated Images are ..." ?></h1>
+
+        <?php
+        echo "<img src='https://statusneo.com/wp-content/uploads/2023/02/MicrosoftTeams-image551ad57e01403f080a9df51975ac40b6efba82553c323a742b42b1c71c1e45f1.jpg' width=300 height=300>";
+        ?>
+
+        <p><?php echo "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil natus fuga quos adipisci nobis! Fuga ipsa quae iusto quos distinctio, illum minima laborum autem amet fugit unde aperiam in beatae voluptatibus quidem atque impedit tenetur reiciendis quod assumenda placeat possimus enim deleniti! Nesciunt, animi ratione quam reiciendis accusantium voluptatum, exercitationem doloribus dolor totam tempora hic voluptatem recusandae! Magnam, accusantium eius, alias eos autem et doloribus quae quisquam commodi soluta repellendus quos expedita iure fugit? Ipsam dolorum distinctio, vitae quia hic repellendus asperiores molestiae id rerum ullam, autem necessitatibus voluptas, reprehenderit voluptatibus incidunt aut tempore sunt eaque cumque. Magnam, alias ex!" ?></p>
+    </div>
+</body>
+
+</html>
