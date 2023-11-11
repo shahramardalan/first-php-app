c9022fbd1c337982d6e81cb5e78e7856b3923c3f
shahram <ardalan.shahram@gmail.com>
2023-11-11 20:58:21 +0330

    change php and css code

--- app.php
+++ app.php
@@ -13,7 +13,7 @@
         <h1><?php echo "How AI-Generated Images are ..." ?></h1>
 
         <?php
-        echo "<img src='https://statusneo.com/wp-content/uploads/2023/02/MicrosoftTeams-image551ad57e01403f080a9df51975ac40b6efba82553c323a742b42b1c71c1e45f1.jpg' width=300 height=300>";
+        echo "<img class='image' src='https://statusneo.com/wp-content/uploads/2023/02/MicrosoftTeams-image551ad57e01403f080a9df51975ac40b6efba82553c323a742b42b1c71c1e45f1.jpg'>";
         ?>
 
         <p><?php echo "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil natus fuga quos adipisci nobis! Fuga ipsa quae iusto quos distinctio, illum minima laborum autem amet fugit unde aperiam in beatae voluptatibus quidem atque impedit tenetur reiciendis quod assumenda placeat possimus enim deleniti! Nesciunt, animi ratione quam reiciendis accusantium voluptatum, exercitationem doloribus dolor totam tempora hic voluptatem recusandae! Magnam, accusantium eius, alias eos autem et doloribus quae quisquam commodi soluta repellendus quos expedita iure fugit? Ipsam dolorum distinctio, vitae quia hic repellendus asperiores molestiae id rerum ullam, autem necessitatibus voluptas, reprehenderit voluptatibus incidunt aut tempore sunt eaque cumque. Magnam, alias ex!" ?></p>
